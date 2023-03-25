a69b701860e6aec617107ac7682183385d513802
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2023-03-25 17:20:54 +0200

    changes on frontend

---
+++ components/Nutritionist/NutrScheduler.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface NutrSchedulerProps {}
+
+const NutrScheduler: React.FC<NutrSchedulerProps> = ({}) => {
+  return (
+    <section id="section_4" className=" h-screen">
+      <div></div>
+    </section>
+  );
+};
+
+export default NutrScheduler;
