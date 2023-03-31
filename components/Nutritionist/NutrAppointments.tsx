1383d5a1b9be1641e51075682afa4b83a1c5f04e
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2023-03-31 04:55:53 +0300

    small changes

--- components/Nutritionist/NutrAppointments.tsx
+++ components/Nutritionist/NutrAppointments.tsx
@@ -62,6 +62,7 @@ const NutrAppointments: React.FC<NutrAppointmentsProps> = ({}) => {
   );
 
   return (
+    // changing test
     <section id="section_3" className="grid min-h-screen  bg-myGrey-200">
       <div className="">
         <h1 className="relative z-[2] pt-6 text-center text-2xl font-bold text-white  hover:text-myRed">
