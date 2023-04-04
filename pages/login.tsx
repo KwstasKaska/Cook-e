e2b3401a05653e2479bf451544e0792a9ed54735
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2023-04-04 17:11:24 +0300

    login fully responsive

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,92 +3,108 @@ import { NextPage } from 'next';
 import chef from '/public/images/chef.png';
 import logo from '/public/images/5.png';
 import Link from 'next/link';
+import { useRef, useEffect } from 'react';
 
 const Login: NextPage = ({}) => {
-  return (
-    // Εδώ βρίσκεται το section με τις δύο εικόνες, δηλαδή το αριστερό section.
+  const div1Ref = useRef<HTMLDivElement>(null);
+  const div2Ref = useRef<HTMLDivElement>(null);
 
-    <main className="flex h-screen items-center justify-center bg-myBeige-100 text-white md:block">
-      <div className="border-2 border-red-500 md:grid md:h-screen  md:grid-cols-2">
-        <section className="hidden md:relative   md:block   md:min-h-full   md:bg-myBeige-100">
-          <div className="rounded-l-[3.125rem] border-[5px] border-myBlue-200  bg-myBeige-100 md:absolute md:right-0 md:top-[10%] md:grid md:w-full   md:max-w-[600px] md:grid-flow-row md:justify-center ">
-            <Image
-              src={logo}
-              alt={'logo image'}
-              height="76"
-              width="198"
-              className=""
-            ></Image>
+  useEffect(() => {
+    const div1Height = div1Ref.current?.offsetHeight;
+    if (div1Height != null) {
+      div2Ref.current!.style.height = `${div1Height}px`;
+    }
+  }, []);
+
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center  bg-myBeige-100 text-white md:flex-row md:items-stretch md:justify-items-stretch  ">
+      {/* Εδώ βρίσκεται το section με τις δύο εικόνες, δηλαδή το αριστερό section */}
+      <section className="hidden md:flex md:flex-1 md:flex-col  md:items-end  md:justify-center md:bg-myBeige-100">
+        <div
+          ref={div2Ref}
+          className="rounded-l-[3.125rem]  border-[5px] border-myBlue-200 bg-myBeige-100  md:relative    md:w-full md:max-w-[600px]  "
+        >
+          <Image
+            src={logo}
+            alt={'logo image'}
+            height="76"
+            width="198"
+            className="md:absolute md:left-0 md:top-0"
+          ></Image>
+          <div className="flex h-full items-end justify-center">
             <Image
               src={chef}
               alt={'chef image'}
-              width="342"
-              height="490"
-              className="md:rounded-[3.125rem]"
+              width="340"
+              height="300"
+              className=" md:rounded-[3.125rem]"
             ></Image>
           </div>
-        </section>
+        </div>
+      </section>
 
-        {/* Εδώ βρίσκεται το section με την φόρμα εισαγωγής στοιχείων και τα κέιμενα, δηλαδή το δεξιά section. */}
+      {/* Εδώ βρίσκεται το section με την φόρμα εισαγωγής στοιχείων και τα κέιμενα, δηλαδή το δεξιά section. */}
 
-        <section className=" md:relative    md:bg-myBlue-100">
-          <div className="relative grid max-w-[350px]  grid-flow-row  justify-items-center gap-[1.5em] rounded-[1.5625em] bg-myBlue-200 md:absolute md:left-0  md:top-[10%] md:w-full   md:max-w-[600px] md:rounded-r-[3.125rem] md:rounded-l-[0px]  md:py-[2.2em] lg:py-[.8em]">
-            <Image
-              src={logo}
-              alt={'logo image'}
-              height="54"
-              width="143"
-              className="absolute justify-self-start md:hidden"
-            ></Image>
-            <h1 className="mt-[2.5em] font-exo text-xl font-bold md:mt-[1.5em] md:text-3xl lg:text-4xl">
-              Καλωσήρθες πίσω!
-            </h1>
+      <section className="md:flex md:flex-1 md:flex-col md:justify-center  md:bg-myBlue-100">
+        <div
+          ref={div1Ref}
+          className="relative grid h-fit  max-w-[350px]  grid-flow-row  justify-items-center gap-[1.5em] rounded-[1.5625em] border-[5px] border-myBlue-200 bg-myBlue-200 py-6 md:w-full md:max-w-[600px]  md:rounded-r-[3.125rem] md:rounded-l-[0px] lg:border-none"
+        >
+          <Image
+            src={logo}
+            alt={'logo image'}
+            height="54"
+            width="143"
+            className="absolute justify-self-start md:hidden"
+          ></Image>
+          <h1 className="mt-[2.5em] font-exo text-xl font-bold md:mt-0 md:text-3xl lg:text-4xl">
+            Καλωσήρθες πίσω!
+          </h1>
 
-            <span className="-mt-[1em] font-source text-base font-normal md:text-xl  ">
-              Έτοιμος για μαγειρική;
+          <span className="-mt-[1em] font-source text-base font-normal md:text-xl  ">
+            Έτοιμος για μαγειρική;
+          </span>
+          <form className="grid grid-flow-row justify-items-center gap-[.8em]">
+            <span className=" mb-[.25em] text-xl font-bold md:text-3xl lg:text-4xl">
+              Είσοδος
             </span>
-            <form className="grid grid-flow-row justify-items-center gap-[.8em]">
-              <span className=" mb-[.25em] text-xl font-bold md:text-3xl lg:text-4xl">
-                Είσοδος
-              </span>
-              <input
-                type="email"
-                className="mx-[1em] w-[17em]  rounded-[0.5rem] text-black placeholder:text-base placeholder:italic placeholder:text-myBlue-200 lg:placeholder:text-xl "
-                placeholder="Διεύθυνση Email"
-              />
+            <input
+              type="email"
+              className="mx-[1em] w-[17em]  rounded-[0.5rem] text-black placeholder:text-base placeholder:italic placeholder:text-myBlue-200 lg:placeholder:text-xl "
+              placeholder="Διεύθυνση Email"
+            />
 
-              <input
-                type="password"
-                className="w-[17em] rounded-[0.5rem] text-black  placeholder:text-base placeholder:italic placeholder:text-myBlue-200 lg:placeholder:text-xl"
-                placeholder="Κωδικός Πρόσβασης"
-              />
+            <input
+              type="password"
+              className="w-[17em] rounded-[0.5rem] text-black  placeholder:text-base placeholder:italic placeholder:text-myBlue-200 lg:placeholder:text-xl"
+              placeholder="Κωδικός Πρόσβασης"
+            />
 
-              <button
-                className="  w-[14em] rounded-[.5rem] bg-myRed py-1 text-base font-normal  lg:text-xl"
-                type="submit"
-              >
-                Είσοδος
-              </button>
-              <Link
-                className="justify-self-center font-source text-base font-normal underline md:text-xl "
-                href="#"
-              >
-                Ξεχάσατε τον κωδικό;
-              </Link>
-            </form>
+            <button
+              className="  w-[14em] rounded-[.5rem] bg-myRed py-1 text-base font-normal  lg:text-xl"
+              type="submit"
+            >
+              Είσοδος
+            </button>
+            <Link
+              className="justify-self-center font-source text-base font-normal underline md:text-xl "
+              href="#"
+            >
+              Ξεχάσατε τον κωδικό;
+            </Link>
+          </form>
 
-            <p className="mt-[2em]  font-source text-base md:text-xl  ">
-              Δεν έχεις ακόμα λογαριασμό;
-              <Link
-                className="block text-center font-source text-xl font-bold underline md:text-3xl "
-                href="#"
-              >
-                Δημιούργησε έναν
-              </Link>
-            </p>
-          </div>
-        </section>
-      </div>
+          <p className="mt-[2em]  font-source text-base md:text-xl  ">
+            Δεν έχεις ακόμα λογαριασμό;
+            <Link
+              className="block text-center font-source text-xl font-bold underline md:text-3xl "
+              href="#"
+            >
+              Δημιούργησε έναν
+            </Link>
+          </p>
+        </div>
+      </section>
     </main>
   );
 };
