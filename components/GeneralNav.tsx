ea642e8b936a7a08470868067e98d3a6ebc7f16f
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2023-05-13 16:47:06 +0300

    changes

--- components/GeneralNav.tsx
+++ components/GeneralNav.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 const GeneralNav: React.FC = () => {
   return (
     <nav className="  text-black">
-      <ul className=" grid  grid-flow-col justify-evenly text-9 font-bold uppercase    md:text-sm xl:text-xl">
+      <ul className=" grid  grid-flow-col justify-evenly text-9 font-bold     md:text-sm xl:text-xl">
         <li className="">
-          <a href="#">Αρχικη</a>
+          <a href="#">Αρχική</a>
         </li>
         <li>
-          <a href="#">Χρηστης</a>
+          <a href="#">Χρήστης</a>
         </li>
         <li>
           <a href="#">Chef</a>
         </li>
         <li>
-          <a href="#">Διατροφολογος</a>
+          <a href="#">Διατροφολόγος</a>
         </li>
       </ul>
     </nav>
