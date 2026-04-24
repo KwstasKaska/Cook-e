78bdbf6a40dc03c44931db42debb4b2911aaa0fa
KwstasKaska <round9876@gmail.com>
2026-04-24 16:25:28 +0300

    inserted a embeded accessibility feature

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="h-full">
-      <Head></Head>
+      <Head>
+        <script src="https://app.embed.im/accessibility.js" defer />
+      </Head>
       <body className="min-h-full">
         <Main />
         <NextScript />
