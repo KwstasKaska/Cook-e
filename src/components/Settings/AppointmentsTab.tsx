9ee38fde9f42cc7f182771f29f5a26226fb42ed1
KwstasKaska <round9876@gmail.com>
2026-05-08 15:51:06 +0300

    fetch policy added

--- src/components/Settings/AppointmentsTab.tsx
+++ src/components/Settings/AppointmentsTab.tsx
@@ -26,7 +26,9 @@ const AppointmentsTab: React.FC = () => {
   const dateFnsLocale = i18n.language === 'el' ? el : enUS;
   const [page, setPage] = useState(0);
 
-  const { data, loading } = useMyAppointmentRequestsQuery();
+  const { data, loading } = useMyAppointmentRequestsQuery({
+    fetchPolicy: 'network-only',
+  });
   const all = data?.myAppointmentRequests ?? [];
 
   const totalPages = Math.ceil(all.length / LIMIT);
