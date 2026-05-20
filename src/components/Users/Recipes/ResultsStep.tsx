328f482ae9c549db4513273e75c94ba9884694f7
KwstasKaska <round9876@gmail.com>
2026-05-20 04:39:21 +0300

    fixed the mobile version

--- src/components/Users/Recipes/ResultsStep.tsx
+++ src/components/Users/Recipes/ResultsStep.tsx
@@ -86,7 +86,7 @@ export default function ResultsStep({
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-3 ">
         {suggestions.map((s) => (
           <SuggestionCard
             key={s.recipe.id}
