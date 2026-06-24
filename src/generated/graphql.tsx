115ee6f0220fbbf28c1208b5028b1d465bb69d88
KwstasKaska <round9876@gmail.com>
2026-06-24 12:21:22 +0300

    removed unused

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -253,7 +253,6 @@ export type Mutation = {
   addToCart: ShoppingCart;
   cancelAcceptedRequest: Scalars['Boolean']['output'];
   cancelAcceptedRequestAsNutr: Scalars['Boolean']['output'];
-  changePassword: UserResponse;
   clearCart: Scalars['Boolean']['output'];
   createAppointment: AppointmentResponse;
   createArticle: ArticleResponse;
@@ -314,12 +313,6 @@ export type MutationCancelAcceptedRequestArgs = {
 
 export type MutationCancelAcceptedRequestAsNutrArgs = {
   id: Scalars['Int']['input'];
-};
-
-
-export type MutationChangePasswordArgs = {
-  newPassword: Scalars['String']['input'];
-  token: Scalars['String']['input'];
 };
 
 
@@ -1247,14 +1240,6 @@ export type ClearCartMutationVariables = Exact<{ [key: string]: never; }>;
 
 
 export type ClearCartMutation = { __typename?: 'Mutation', clearCart: boolean };
-
-export type ChangePasswordMutationVariables = Exact<{
-  token: Scalars['String']['input'];
-  newPassword: Scalars['String']['input'];
-}>;
-
-
-export type ChangePasswordMutation = { __typename?: 'Mutation', changePassword: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null, user?: { __typename?: 'User', id: number, username: string, email: string, role: UserRole, image?: string | null } | null } };
 
 export type CreateRecipeMutationVariables = Exact<{
   data: CreateRecipeInput;
@@ -2809,40 +2794,6 @@ export function useClearCartMutation(baseOptions?: Apollo.MutationHookOptions<Cl
 export type ClearCartMutationHookResult = ReturnType<typeof useClearCartMutation>;
 export type ClearCartMutationResult = Apollo.MutationResult<ClearCartMutation>;
 export type ClearCartMutationOptions = Apollo.BaseMutationOptions<ClearCartMutation, ClearCartMutationVariables>;
-export const ChangePasswordDocument = gql`
-    mutation ChangePassword($token: String!, $newPassword: String!) {
-  changePassword(token: $token, newPassword: $newPassword) {
-    ...RegularUserResponse
-  }
-}
-    ${RegularUserResponseFragmentDoc}`;
-export type ChangePasswordMutationFn = Apollo.MutationFunction<ChangePasswordMutation, ChangePasswordMutationVariables>;
-
-/**
- * __useChangePasswordMutation__
- *
- * To run a mutation, you first call `useChangePasswordMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useChangePasswordMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [changePasswordMutation, { data, loading, error }] = useChangePasswordMutation({
- *   variables: {
- *      token: // value for 'token'
- *      newPassword: // value for 'newPassword'
- *   },
- * });
- */
-export function useChangePasswordMutation(baseOptions?: Apollo.MutationHookOptions<ChangePasswordMutation, ChangePasswordMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<ChangePasswordMutation, ChangePasswordMutationVariables>(ChangePasswordDocument, options);
-      }
-export type ChangePasswordMutationHookResult = ReturnType<typeof useChangePasswordMutation>;
-export type ChangePasswordMutationResult = Apollo.MutationResult<ChangePasswordMutation>;
-export type ChangePasswordMutationOptions = Apollo.BaseMutationOptions<ChangePasswordMutation, ChangePasswordMutationVariables>;
 export const CreateRecipeDocument = gql`
     mutation CreateRecipe($data: CreateRecipeInput!) {
   createRecipe(data: $data) {
